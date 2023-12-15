45fcb50c96c6c03bc16fe6102206a35f4ad857f4
Kartik Patel <kartik@git.com>
2023-12-14 21:26:09 -0500

    First Commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors=require('cors');
 const app=express();
 const sqlite3=require('sqlite3').verbose();
 const db=new sqlite3.Database('NotepadDatabase.db');
+let UName="";
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
@@ -28,20 +29,31 @@ app.post('/signup',function(req,responce){
     console.log("enter signup")
     if(password==cpassword)
     {
-        console.log("password match")
         db.all("SELECT * FROM Users WHERE username=?",username,function(err,output){
-            console.log(output)
             if(output[0]!=null)
             {
-                console.log("username exist")
-                responce.send({message:"This username is already taken!!",color:"red"});
+                db.all("SELECT * FROM Users WHERE username=? AND password=?",username,password,function(err,output){
+                    if(output[0]!=null)
+                    {
+                        responce.send({message:"This account has already been registered!!",color:"gold"});
+                    }
+                    else
+                    {
+                        responce.send({message:"This username is already taken!!",color:"red"});
+                    }
+                });
             }
             else
             {
                 db.run("INSERT INTO Users(username,password) VALUES (?,?)",username,password,function(err,result){
                     if(err==null)
                     {
-                        responce.send({message:"User has been registered successfully.",color:"green"});
+                        db.run("CREATE TABLE ?(key TEXT,value TEXT)",username,function(error,res){
+                            if(error==null)
+                            {
+                                responce.send({message:"User has been registered successfully.",color:"green"});
+                            }
+                        })
                     }
                 })
             }
@@ -49,9 +61,24 @@ app.post('/signup',function(req,responce){
     }
     else
     {
-        console.log("password not match")
         responce.send({message:"Confirm Password does not match with the Password!",color:"red"});
     }
+})
+
+app.post('/login',function(req,response){
+    let username=req.body.username;
+    let password=req.body.password;
+    db.all("SELECT * FROM Users WHERE username=? AND password=?",username,password,function(error,output){
+        if(output[0]!=null)
+        {
+            UName=username;
+            response.send({message:"Username "+username+" has been logged in successfully.",color:"green"});
+        }
+        else
+        {
+            response.send({message:"Username and/or Password is incorrect!",color:"red"});
+        }
+    })
 })
 
 const server=app.listen(process.env.PORT,function(){
