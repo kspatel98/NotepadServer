54d0ee7dd6f9834a1fb56169fff2615859919671
Kartik Patel <kartik@git.com>
2024-01-02 18:48:52 -0500

    First Commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors({
     origin: '*'
 }));
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://notepad-gules-eight.vercel.app/");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
