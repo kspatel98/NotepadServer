8effcc2cba98a809a974c8cb8e304cc2e5a04539
Kartik Patel <kartik@git.com>
2024-01-02 21:41:29 -0500

    First Commit

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ const db = new sqlite3.Database('NotepadDatabase.db');
 let UName = "";
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({
-    origin: '*'
-}));
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-})
+// app.use(cors({
+//     origin: '*'
+// }));
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//     next();
+// })
 cron.schedule('*/14 * * * *', function() {
     console.log ('Restarting server');
 https
