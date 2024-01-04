ecb83eb4b8cd59557e5b076fb5b340991496d8ee
Kartik Patel <kartik@git.com>
2024-01-04 18:35:13 -0500

    First Commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors({
 }));
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
 })
