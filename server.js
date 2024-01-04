aa9eee5c54a758ccbf9489e5e193e8e93406e16b
Kartik Patel <kartik@git.com>
2024-01-04 18:42:48 -0500

    First Commit

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors({
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization,X-Requested-With");
     next();
 })
 cron.schedule('*/14 * * * *', function() {
