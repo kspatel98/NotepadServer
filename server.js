ecf16bcdefd2e700d13d077121d58a07125f2735
Kartik Patel <kartik@git.com>
2024-01-09 20:48:29 -0500

    First Commit

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.post('/login', function (req, response) {
 app.post('/save', function (req, response) {
     let filename = req.body.filename;
     let content = req.body.content;
-    let statement="SELECT * FROM "+UName+" WHERE key='"+filename+"'";
+    let statement="SELECT * FROM "+UName+" WHERE key="+filename;
     db.all(statement,function(error,output){
         if(output!=null)
         {
